dc00adee37426d7a2daea9442b231705c25a308b
RandyDeo <randydeo@gmail.com>
2021-10-11 11:14:08 -0400

    Added Remaining properties + tests Fixed existing tests

--- amorphous_entity.go
+++ amorphous_entity.go
@@ -23,11 +23,9 @@ type BasicProperty struct {
 func (b *BasicProperty) GetType() string {
 	return b.Type
 }
-
 func (b *BasicProperty) GetLabel() string {
 	return b.Label
 }
-
 func (b *BasicProperty) GetErrors() []error {
 	return b.errors
 }
@@ -44,14 +42,18 @@ func (s *StringProperty) IsValid() bool {
 		s.errors = append(s.errors, fmt.Errorf("'%s' is required", s.Label))
 		return false
 	}
-
 	return true
 }
 
 //FromLabelAndValue create property using label
 func (s *StringProperty) FromLabelAndValue(label string, value string, isRequired bool) *StringProperty {
-	s.Label = label
-	s.IsRequired = isRequired
+	if s.BasicProperty == nil {
+		s.BasicProperty = &BasicProperty{}
+	}
+	s.BasicProperty.Type = "string"
+	s.BasicProperty.Label = label
+	s.Value = value
+	s.BasicProperty.IsRequired = isRequired
 	return s
 }
 
@@ -61,21 +63,64 @@ type BooleanProperty struct {
 	Value bool `json:"value"`
 }
 
+//IsValid add rules for validating value
+func (b *BooleanProperty) IsValid() bool {
+	return true
+}
+
+//FromLabelAndValue create property using label
+func (b *BooleanProperty) FromLabelAndValue(label string, value bool, isRequired bool) *BooleanProperty {
+	if b.BasicProperty == nil {
+		b.BasicProperty = &BasicProperty{}
+	}
+	b.BasicProperty.Type = "boolean"
+	b.BasicProperty.Label = label
+	b.Value = value
+	b.BasicProperty.IsRequired = isRequired
+	return b
+}
+
 //NumericProperty basic string property
 type NumericProperty struct {
 	*BasicProperty
 	Value float32 `json:"value"`
 }
 
+//IsValid add rules for validating value
+func (n *NumericProperty) IsValid() bool {
+	if n.IsRequired && n.Value == 0 {
+		n.errors = append(n.errors, fmt.Errorf("'%s' is required", n.Label))
+		return false
+	}
+	return true
+}
+
+//FromLabelAndValue create property using label
+func (n *NumericProperty) FromLabelAndValue(label string, value float32, isRequired bool) *NumericProperty {
+	if n.BasicProperty == nil {
+		n.BasicProperty = &BasicProperty{}
+	}
+	n.BasicProperty.Type = "numeric"
+	n.BasicProperty.Label = label
+	n.Value = value
+	n.BasicProperty.IsRequired = isRequired
+	return n
+}
+
 type AmorphousEntity struct {
 	*BasicEntity
-	properties map[string]Property `json:"properties"`
+	Properties map[string]Property `json:"properties"`
 }
 
 func (e *AmorphousEntity) Get(label string) Property {
-	return e.properties[label]
+	return e.Properties[label]
 }
-
 func (e *AmorphousEntity) Set(property Property) {
-	e.properties[property.GetLabel()] = property
+	if e == nil {
+		e = &AmorphousEntity{}
+	}
+	if e.Properties == nil {
+		e.Properties = make(map[string]Property)
+	}
+	e.Properties[property.GetLabel()] = property
 }
--- amorphous_entity_test.go
+++ amorphous_entity_test.go
@@ -1,8 +1,9 @@
 package weos_test
 
 import (
-	"github.com/wepala/weos"
 	"testing"
+
+	"github.com/wepala/weos"
 )
 
 type user struct {
@@ -11,6 +12,9 @@ type user struct {
 
 func TestAmorphousEntity_StringProperty(t *testing.T) {
 	admin := new(user)
+	if admin.AmorphousEntity == nil {
+		admin.AmorphousEntity = &weos.AmorphousEntity{}
+	}
 	t.Run("add property", func(t *testing.T) {
 		admin.Set(new(weos.StringProperty).FromLabelAndValue("FirstName", "Eric", false))
 		property := admin.Get("FirstName")
@@ -29,17 +33,42 @@ func TestAmorphousEntity_StringProperty(t *testing.T) {
 		}
 	})
 }
-
 func TestAmorphousEntity_BooleanProperty(t *testing.T) {
 	admin := new(user)
+	if admin.AmorphousEntity == nil {
+		admin.AmorphousEntity = &weos.AmorphousEntity{}
+	}
 	t.Run("add property", func(t *testing.T) {
 		admin.Set(new(weos.BooleanProperty).FromLabelAndValue("is trinidadian", true, false))
-		property := admin.Get("FirstName")
+		property := admin.Get("is trinidadian")
 		if property.GetType() != "boolean" {
 			t.Errorf("expected type to be '%s', got '%s'", "boolean", property.GetType())
 		}
-		if property.(*weos.BooleanProperty).Value {
+		if !property.(*weos.BooleanProperty).Value {
 			t.Errorf("expected '%s' to be true", property.GetLabel())
+		}
+	})
+}
+func TestAmorphousEntity_NumericProperty(t *testing.T) {
+	admin := new(user)
+	if admin.AmorphousEntity == nil {
+		admin.AmorphousEntity = &weos.AmorphousEntity{}
+	}
+	t.Run("add property", func(t *testing.T) {
+		admin.Set(new(weos.NumericProperty).FromLabelAndValue("amount", 100, false))
+		property := admin.Get("amount")
+		if property.GetType() != "numeric" {
+			t.Errorf("expected type to be '%s', got '%s'", "numeric", property.GetType())
+		}
+		if property.(*weos.NumericProperty).Value != 100 {
+			t.Errorf("expected value to be '%d', got '%f'", 100, property.(*weos.NumericProperty).Value)
+		}
+	})
+	t.Run("invalid numeric property", func(t *testing.T) {
+		admin.Set(new(weos.NumericProperty).FromLabelAndValue("amount", 0, true))
+		property := admin.Get("amount")
+		if property.IsValid() {
+			t.Fatalf("expected '%s' property to be invalid", property.GetLabel())
 		}
 	})
 }
