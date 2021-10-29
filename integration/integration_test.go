64c207d782fc670602b5e452b58106d8d855ec50
RandyDeo <randydeo@gmail.com>
2021-10-29 15:17:38 -0400

    adding endpoint discovery

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -152,7 +152,8 @@ func TestMain(m *testing.M) {
 				AccessKeyID:     "fakeMyKeyId",
 				SecretAccessKey: "fakeSecretAccessKey",
 			}),
-			Endpoint: aws.String("http://localhost:8000"),
+			Endpoint:                aws.String("http://localhost:8000"),
+			EnableEndpointDiscovery: aws.Bool(true),
 		})
 		if err != nil {
 			log.Fatalf("Could not setup dynamoDB: %s", err.Error())
